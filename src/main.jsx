75454306106f6841dae950f78cd74129031ea677
DiggDiggs <christopherdiggs1@gmail.com>
2024-08-28 16:18:50 +0000

    task-0

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,13 @@
+import ReactDOM from "react-dom/client";
 import "./index.css";
+import App from "./App";
+import { StrictMode } from "react";
 
 // Render the App component to the root element
+const div = document.getElementById("root");
+const root = ReactDOM.createRoot(div);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
